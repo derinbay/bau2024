143583d78b43f5a1f539fadf14c79ade47b5b124
Taylan Derinbay <taylan.derinbay@trendyol.com>
2023-12-01 18:35:22 +0300

    add new testcase

--- src/test/java/com/bau/qa/BasketTest.java
+++ src/test/java/com/bau/qa/BasketTest.java
@@ -1,5 +1,11 @@
 package com.bau.qa;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+import static com.bau.qa.BaseTest.getDriver;
+
 public class BasketTest {
 
     /**
@@ -8,4 +14,14 @@ public class BasketTest {
      * 3- add to basket
      * 4- assert that the product has beed added to basket
      * */
+
+    public static void main(String[] args) {
+        WebElement searchBar = getDriver().findElement(By.id("searchData"));
+        searchBar.sendKeys("tshirt");
+        WebElement searchButton = getDriver().findElement(By.className("searchBtn"));
+        searchButton.click();
+
+        getDriver().findElement(By.id("searchData")).sendKeys("tshirt" + Keys.ENTER);
+    }
+
 }
